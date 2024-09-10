c2c58598454c4b2c0406432bae035c0a84388503
LOUNNACI-Sarah <166284922+LOUNNACI-Sarah@users.noreply.github.com>
2024-09-10 14:09:23 +0100

    Add files via upload

---
+++ lab1 UART using PL2303/lab1/firmware/lab1.X/build/default/production/_ext/1408546951/sys_debug.o.d
@@ -0,0 +1,12 @@
+build/default/production/_ext/1408546951/sys_debug.o:  \
+ ../../../../framework/system/debug/src/sys_debug.c  \
+ ../src/system_config/default/system_config.h  \
+ ../../../../framework/system/system.h  \
+ ../../../../framework/system/common/sys_common.h  \
+ ../../../../framework/system/common/sys_module.h  \
+ ../../../../framework/system/console/sys_console.h  \
+ ../../../../framework/system/common/sys_queue.h  \
+ ../../../../framework/system/console/src/sys_console_local.h  \
+ ../../../../framework/driver/driver_common.h  \
+ ../../../../framework/system/debug/src/sys_debug_local.h  \
+ ../../../../framework/system/debug/sys_debug.h 
